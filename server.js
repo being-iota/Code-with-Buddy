bcde438e2633bf0ce253c0907ad58751f0f13ad6
Vardhman Gupta <vardhmangupta2004@gmail.com>
2025-02-20 12:18:17 +0530

    updated

--- server.js
+++ server.js
@@ -80,5 +80,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
